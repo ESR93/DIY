bd9c69b3c36cb6a2679f6ab82038f991205cfdb3
Evgeny <esryabchenkov@gmail.com>
2019-11-14 18:10:46 +0100

    added feature of adding the labeles

--- routes/dashboard_sneaker.js
+++ routes/dashboard_sneaker.js
@@ -8,28 +8,41 @@ router.get("/prod-add", (req, res) => {
   console.log(res);
   tagModel.find()
   .then(dbRes => {
-    res.render("products_add",{tag:dbRes})
+    res.render("products_add",{tags:dbRes})
   })
   .catch(err => console.log(err));;
 });
 
 router.post("/prod-add", (req, res) => {
   const sneak = req.body
-  const newSneaker = {
-    name: sneak.name,
-    ref: sneak.ref,
-    sizes: sneak.size,
-    description: sneak.descr,
-    price: sneak.price,
-    category: sneak.category,
-    tag: sneak.id_tags
-  };
-  sneakerModel
-    .create(newSneaker)
+  if (sneak.name) {
+    const newSneaker = {
+      name: sneak.name,
+      ref: sneak.ref,
+      sizes: sneak.size,
+      description: sneak.descr,
+      price: sneak.price,
+      category: sneak.category,
+      tag: sneak.id_tags
+    };
+    sneakerModel
+      .create(newSneaker)
+      .then(() => {
+        res.redirect("/sneakers/collection");
+      })
+      .catch(dbErr => console.error(dbErr));  
+  }
+  else if (sneak.label) {
+    const newTag = {
+      label: sneak.label
+    }
+    tagModel
+    .create(newTag)
     .then(() => {
-      res.redirect("/");
+      res.redirect("/prod-add");
     })
-    .catch(dbErr => console.error(dbErr));
+    .catch(dbErr => console.error(dbErr));  
+  }
 });
 
 module.exports = router;
@@ -41,6 +54,12 @@ router.get("/prod-manage", (req, res) => {
   })
 });
 
-router.get("/prod-edit", (req, res) => {
-  res.render("product_edit")
+router.get("/product-edit/:id", (req, res) => {
+  const sneaker = sneakerModel.findOne({ _id: req.params.id })
+  const tags = tagModel.find();
+  Promise.all([sneaker, tags])
+  .then(dbRes => {
+    console.log(dbRes)
+    res.render("product_edit",{sneaker:dbRes[0], tags:dbRes[1]})
+  })
 });
