5f4b38615dd5926195bf1b9186b9a5ae65eba417
Louise Marissal <louisemarissal@MacBook-Pro-de-Louise.local>
2019-11-14 18:24:16 +0100

    tags bug fixed

--- routes/dashboard_sneaker.js
+++ routes/dashboard_sneaker.js
@@ -6,16 +6,18 @@ const router = express.Router(); // create an app sub-module (router)
 
 router.get("/prod-add", (req, res) => {
   console.log(res);
-  tagModel.find()
-  .then(dbRes => {
-    res.render("products_add",{tags:dbRes})
-  })
-  .catch(err => console.log(err));;
+  tagModel
+    .find()
+    .then(dbRes => {
+      res.render("products_add", { tags: dbRes });
+    })
+    .catch(err => console.log(err));
 });
 
 router.post("/prod-add", (req, res) => {
-  const sneak = req.body
+  const sneak = req.body;
   if (sneak.name) {
+    console.log(sneak.id_tags);
     const newSneaker = {
       name: sneak.name,
       ref: sneak.ref,
@@ -23,43 +25,41 @@ router.post("/prod-add", (req, res) => {
       description: sneak.descr,
       price: sneak.price,
       category: sneak.category,
-      tag: sneak.id_tags
+      id_tags: sneak.id_tags
     };
+    console.log(newSneaker);
     sneakerModel
       .create(newSneaker)
       .then(() => {
         res.redirect("/sneakers/collection");
       })
-      .catch(dbErr => console.error(dbErr));  
-  }
-  else if (sneak.label) {
+      .catch(dbErr => console.error(dbErr));
+  } else if (sneak.label) {
     const newTag = {
       label: sneak.label
-    }
+    };
     tagModel
-    .create(newTag)
-    .then(() => {
-      res.redirect("/prod-add");
-    })
-    .catch(dbErr => console.error(dbErr));  
+      .create(newTag)
+      .then(() => {
+        res.redirect("/prod-add");
+      })
+      .catch(dbErr => console.error(dbErr));
   }
 });
 
 module.exports = router;
 
 router.get("/prod-manage", (req, res) => {
-  sneakerModel.find()
-  .then(dbRes => {
-    res.render("products_manage",{sneakers:dbRes})
-  })
+  sneakerModel.find().then(dbRes => {
+    res.render("products_manage", { sneakers: dbRes });
+  });
 });
 
 router.get("/product-edit/:id", (req, res) => {
-  const sneaker = sneakerModel.findOne({ _id: req.params.id })
+  const sneaker = sneakerModel.findOne({ _id: req.params.id });
   const tags = tagModel.find();
-  Promise.all([sneaker, tags])
-  .then(dbRes => {
-    console.log(dbRes)
-    res.render("product_edit",{sneaker:dbRes[0], tags:dbRes[1]})
-  })
+  Promise.all([sneaker, tags]).then(dbRes => {
+    console.log(dbRes);
+    res.render("product_edit", { sneaker: dbRes[0], tags: dbRes[1] });
+  });
 });
