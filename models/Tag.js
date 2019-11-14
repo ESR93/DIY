43dd78d01e85e3783ccf665a25373043af3f3097
Louise Marissal <louisemarissal@MacBook-Pro-de-Louise.local>
2019-11-14 10:39:15 +0100

    models are done

--- models/Tag.js
+++ models/Tag.js
@@ -1,1 +1,10 @@
-const mongoose = require("");
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const tagSchema = new Schema({
+  label: String
+});
+
+const tagModel = mongoose.Model("Tag", tagSchema);
+module.exports = tagModel;
